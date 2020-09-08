30e96ea67fc03541ce781bd3a0356b852afb15fe
John <johncferguson90@gmail.com>
2020-09-07 20:39:57 -0600

    created cli-tool

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const chalk = require("chalk")
+const clear = require("clear")
+const figlet = require("figlet")
+const files = require("./lib/files")
+const github = require("./lib/github")
+const project = require("./lib/project")
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('R2-D2', { horizontalLayout: 'full'})
+    )
+)
+
+/*if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a local Git Repo!'));
+    process.exit();
+}*/
+
+const run = async () => {
+    project.createLocalProject();
+}
+
+run();
+
+const runGithub = async () => {
+    let token = github.getStoredGithubToken();
+    if (!token) {
+        token = await github.getPersonalAccessToken();
+    }
+
+    console.log(token)
+}
